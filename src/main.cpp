97f974f050ba7701d295f3bc3a2cf2329f0788b7
Taha Magdy <t.tahamagdyt@outlook.com>
2019-02-08 16:45:32 +0200

    playing with ICU

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,44 @@
 // main.cpp for playing. the library functions will be in other files.
 #include <iostream>
-
-
-#include <unicode/utext.h>
-#include <unicode/ubrk.h>
-//#include <unicode/ustring.h>
-#include <assert.h>
-
-int countWords(const char *utf8String) {
-    UText          *ut        = NULL;
-    UBreakIterator *bi        = NULL;
-    int             wordCount = 0;
-    UErrorCode      status    = U_ZERO_ERROR;
-
-    ut = utext_openUTF8(ut, utf8String, -1, &status);
-    bi = ubrk_open(UBRK_WORD, "en_us", NULL, 0, &status);
-
-    ubrk_setUText(bi, ut, &status);
-    while (ubrk_next(bi) != UBRK_DONE) {
-        if (ubrk_getRuleStatus(bi) != UBRK_WORD_NONE) {
-            /* Count only words and numbers, not spaces or punctuation */
-            wordCount++;
-        }
-    }
-    utext_close(ut);
-    ubrk_close(bi);
-    assert(U_SUCCESS(status));
-    return wordCount;
-}
+#include <fstream>
+#include <unicode/locid.h>
+#include <unicode/regex.h>
 
 
 int main(void) {
-    const char* m = "الحمد لله رب العلمين";
-    std::cout << countWords(m) << " Words" << std::endl;
+
+    std::ifstream infile("/Users/taha/cs/C++/projects/quran/QuranCorpus/quran-uthmani-with-aya-numbers.txt");    
+
+    if(!infile) {
+        std::cout << "Cannot open input file.\n";
+        return 1;
+    }
+
+    std::string line;
+    std::getline(infile, line);
+    std::cout << line << std::endl;
+
+    // * regax
+    UErrorCode status = U_ZERO_ERROR; 
+    RegexMatcher regax_matcher(UnicodeString("\\d+\\|\\d\\|"), 0, status);
+    UnicodeString text("114|6|ﻢِﻧَ ٱﻞْﺠِﻧَّﺓِ ﻭَٱﻞﻧَّﺎﺳِ");
+    regax_matcher.reset(text);
+
+    UnicodeString string_;
+    UnicodeString replacement("");
+
+
+    // find the pattern and replace
+    while (regax_matcher.find(status) && U_SUCCESS(status)) {
+       regax_matcher.appendReplacement(string_, replacement, status);
+    }
+    regax_matcher.appendTail(string_);
+
+    char string__buf[100];
+    int nul_index = string_.extract(0, string_.length(), string__buf, sizeof(string__buf));
+    string__buf[nul_index] = 0;
+    printf("The string_ of find & replace is \"%s\"\n", string__buf);
+
     return 0;
 } // end main
 
